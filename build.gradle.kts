26518454a062a77110352aae07ed5edd60266abb
Michael Kunze <mkunze@inoxio.de>
2022-01-09 10:23:34 +0100

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    id("com.adarshr.test-logger") version "3.1.0"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("jacoco")
+    id("java")
+    id("org.springframework.boot") version "2.6.2"
+}
+
+group = "de.inoxio"
+version = file("version.txt").readText().trim()
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations["annotationProcessor"])
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    compileOnly("org.projectlombok:lombok")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    compileJava {
+        options.apply {
+            isFork = true
+            isIncremental = true
+            encoding = "UTF-8"
+            compilerArgs = mutableListOf("-Xlint", "-Xlint:-processing")
+        }
+    }
+    test {
+        useJUnitPlatform()
+        finalizedBy("jacocoTestReport")
+    }
+    jacocoTestReport {
+        reports { xml.apply { required.set(true) } }
+        dependsOn("test")
+    }
+    dependencyUpdates {
+        rejectVersionIf {
+            listOf("alpha", "beta", "rc", "cr", "m", "preview", "b", "ea", "pr").any { qualifier ->
+                "(?i).*[.-]$qualifier[.\\d-+]*".toRegex().matches(candidate.version)
+            }
+        }
+    }
+    springBoot {
+        bootBuildImage {
+            imageName = "ghcr.io/inoxio/fake-dns:${project.version}"
+            docker {
+                publishRegistry {
+                    username = System.getenv("CI_REGISTRY_USER")
+                    password = System.getenv("CI_REGISTRY_PASSWORD")
+                }
+            }
+        }
+    }
+    wrapper {
+        distributionType = Wrapper.DistributionType.ALL
+        gradleVersion = "7.3.3"
+    }
+}
---
+++ src/main/java/de/inoxio/fakedns/FakeDnsApplication.java
@@ -0,0 +1,12 @@
+package de.inoxio.fakedns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeDnsApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(FakeDnsApplication.class, args);
+	}
+}
